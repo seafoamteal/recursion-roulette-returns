d5ba60d1b766e8eca4349386e8e9cd096a55b92c
Hari Mohan <dev@haripm.com>
2026-03-14 21:47:04 +0000

    optimise polars query

--- py-src/best_airport.py
+++ py-src/best_airport.py
@@ -9,35 +9,32 @@ dest = "DFW"
 route = "-".join(sorted([origin, dest]))
 
 start = time.monotonic()
+base = pl.scan_parquet("data/fd.parquet").with_columns(
+    route=(
+        pl.when(
+            ((pl.col("Origin") == origin) & (pl.col("Dest") == dest))
+            | ((pl.col("Dest") == origin) & (pl.col("Origin") == dest))
+        )
+        .then(True)
+        .otherwise(False)
+    ).alias("route"),
+    flight_code=(pl.concat_str(pl.col("AirlineCode"), pl.col("FlightNumber"))).alias(
+        "flight_code"
+    ),
+)
+
 latest_routes = (
-    pl.scan_parquet("data/fd.parquet")
-    .with_columns(
-        route=(
-            pl.when(pl.col("Origin") < pl.col("Dest"))
-            .then(pl.concat_str(pl.col("Origin"), pl.col("Dest"), separator="-"))
-            .otherwise(pl.concat_str(pl.col("Dest"), pl.col("Origin"), separator="-"))
-        ).alias("route"),
-        flight_code=(
-            pl.concat_str(pl.col("AirlineCode"), pl.col("FlightNumber"))
-        ).alias("flight_code"),
-    )
-    .group_by(pl.col("flight_code"))
+    base.group_by(pl.col("flight_code"))
     .agg(
         pl.col("Date").max().alias("latest_flight"),
         pl.col("route").sort_by(pl.col("Date"), descending=True).first(),
     )
-    .filter((pl.col("route") == route) & (pl.col("latest_flight").ge(date(2025, 1, 1))))
+    .filter((pl.col("route")) & (pl.col("latest_flight").ge(date(2025, 1, 1))))
     .select(pl.col("flight_code"))
 )
 
 lf = (
-    pl.scan_parquet("data/fd.parquet")
-    .with_columns(
-        flight_code=(
-            pl.concat_str(pl.col("AirlineCode"), pl.col("FlightNumber"))
-        ).alias("flight_code"),
-    )
-    .filter(
+    base.filter(
         (pl.col("Cancelled") != 1)
         & (pl.col("Diverted") != 1)
         & (pl.col("ArrDelay").is_not_null())
